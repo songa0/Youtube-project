1346c6c5686b6e371d597adb13ce6e00070007fa
Songa0 <dolphin2440@naver.com>
2021-05-18 22:53:16 +0900

    create new app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <img src='/images/logo.png' alt="logo" />
+        <span>Youtube</span>
+        <input type='text' />
+        <button><i className="fas fa-search"></i></button>
+      </header>
+    </>
+  );
+}
+
+export default App;
