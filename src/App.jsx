f52d3f32a2822f5d8e9c5627eb1415c0d1b6d989
Songa0 <dolphin2440@naver.com>
2021-05-23 22:33:48 +0900

    Add view popular videos

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,16 @@ function App() {
     } 
   };
 
-  const requestParam = {
+  
+  function callSearchAPI(searchKeyWord) {
+
+    const requestParam = {
     apiKey: 'AIzaSyCbbDVhaJVCx_p2g3xUwD6seqhOGzM7lNg',
     q: '',
     part: 'snippet',
-    maxResults : '25'
+    maxResults: '25'
     
   }
-  function callSearchAPI(searchKeyWord) {
     requestParam.q = searchKeyWord ? searchKeyWord : '';
     console.log('searchKeyWord ***', searchKeyWord);
     //`https://www.googleapis.com/youtube/v3/search?part=${requestParam.part}&maxResults=${requestParam.maxResults}&q=${requestParam.q}&key=${requestParam.apiKey}`
@@ -41,8 +43,33 @@ function App() {
       setError(error);
     })
   }
+
+  function callPopularSearchAPI() {
+    const requestParam = {
+    apiKey: 'AIzaSyCbbDVhaJVCx_p2g3xUwD6seqhOGzM7lNg',
+    q: '',
+    part: 'snippet',
+    maxResults: '25',
+    chart :'mostPopular'
+    
+    }
+    
+    //`https://www.googleapis.com/youtube/v3/videos?part=${requestParam.part}&chart=${requestParam.chart}&maxResults=${requestParam.maxResults}&key=${requestParam.apiKey}`
+    fetch(`https://www.googleapis.com/youtube/v3/videos?part=${requestParam.part}&chart=${requestParam.chart}&maxResults=${requestParam.maxResults}&key=${requestParam.apiKey}`, requestOptions)
+    .then(response => {
+      return response.text();
+    })
+    .then((result) => {
+      setVideoData(JSON.parse(result));
+      setIsLoaded(true);
+    }, (error) => {
+      setIsLoaded(true);
+      setError(error);
+    })
+  }
+
 useEffect(() => {
-  callSearchAPI();
+  callPopularSearchAPI();
   },[])
   
   if (error) {
@@ -54,7 +81,10 @@ useEffect(() => {
       <>
         <Header handleClick={callSearchAPI}/>
         <section className='youtube__section'>
-          <article className='video__play__view'></article>
+          <article className='video__play__view'>
+            
+
+          </article>
           <article className='video__list__view'>
             <Videos videos={videoData} />
           </article>
